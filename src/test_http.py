2d8bbb2ce13a3d104c25df76a0ac0e38495cde67
Justin Lange <well1912@gmail.com>
2016-08-16 15:45:10 -0700

    Implement tests for 500 error, rename tests

--- src/test_http.py
+++ src/test_http.py
@@ -3,13 +3,25 @@
 """Test HTTP functions."""
 
 
-def test_response_ok_1():
+def test_response_ok_status():
     from server import response_ok
     lines = response_ok().split('\r\n')
     assert '200 OK' in lines[0]
 
 
-def test_response_ok_2():
+def test_response_ok_header():
     from server import response_ok
     lines = response_ok().split('\r\n')
+    assert any('content-type:' in line.lower() for line in lines)
+
+
+def test_response_error_status():
+    from server import response_ok
+    lines = response_ok().split('\r\n')
+    assert '500 Internal Server Error' in lines[0]
+
+
+def test_response_error_header():
+    from server import response_error
+    lines = response_error().split('\r\n')
     assert any('content-type:' in line.lower() for line in lines)
