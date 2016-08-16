ea6be843467e00c0c33965648bae3de15b5d6cfb
Justin Lange <well1912@gmail.com>
2016-08-16 14:34:15 -0700

    Use list to build message
    to avoid excessive reallocation

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 def recieve_message(conn):
     buffer_length = 8
     message_complete = False
-    message = b''
+    message = []
     while not message_complete:
         part = conn.recv(buffer_length)
-        message += part
+        message.append(part)
         message_complete = len(part) < buffer_length
-    return message
+    return ''.join(message)
 
 
 address = ('127.0.0.1', 5000)
